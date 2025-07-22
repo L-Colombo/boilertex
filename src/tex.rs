2ea3add287834270eb023e35e8bc4e8fb725bc90
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2025-07-22 17:10:38 +0200

    scaffold project

---
+++ src/tex.rs
@@ -0,0 +1,90 @@
+///! This module contains data structure that represent the components
+///! inside of a LaTeX file
+use serde_derive::Deserialize;
+use std::collections::HashMap;
+
+#[derive(Deserialize, Debug, PartialEq, Eq)]
+pub struct TeXDocument {
+    #[serde(rename = "general")]
+    pub general_options: General,
+
+    #[serde(rename = "templates")]
+    pub templates: HashMap<String, TeXTemplate>,
+}
+
+impl TeXDocument {
+    pub fn new(s: &'static str) -> TeXDocument {
+        if let Ok(document) = toml::from_str(s) {
+            document
+        } else {
+            Self::default()
+        }
+    }
+}
+
+impl Default for TeXDocument {
+    fn default() -> Self {
+        TeXDocument {
+            general_options: General {
+                draft: true,
+                main_file_name: "main.tex".to_string(),
+                papersize: "letterpaper".to_string(),
+                textsize: "12pt".to_string(),
+            },
+            templates: HashMap::from([(
+                "example".to_string(),
+                TeXTemplate {
+                    bib_file: Some("bibliography.bib".to_string()),
+                    documentclass: Documentclass {
+                        class: "article".to_string(),
+                        class_opts: None,
+                    },
+                    packages: Some(vec![
+                        TeXPackage {
+                            pkg_name: "fontenc".to_string(),
+                            pkg_opts: Some(vec!["T1".to_string()]),
+                        },
+                        TeXPackage {
+                            pkg_name: "hyperref".to_string(),
+                            pkg_opts: Some(vec![
+                                "hidelinks".to_string(),
+                                "hypertexnames=false".to_string(),
+                            ]),
+                        },
+                        TeXPackage {
+                            pkg_name: "graphicx".to_string(),
+                            pkg_opts: None,
+                        },
+                    ]),
+                },
+            )]),
+        }
+    }
+}
+
+#[derive(Deserialize, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct TeXTemplate {
+    pub bib_file: Option<String>,
+    pub documentclass: Documentclass,
+    pub packages: Option<Vec<TeXPackage>>,
+}
+
+#[derive(Deserialize, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct TeXPackage {
+    pub pkg_name: String,
+    pub pkg_opts: Option<Vec<String>>,
+}
+
+#[derive(Deserialize, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Documentclass {
+    pub class: String,
+    pub class_opts: Option<Vec<String>>,
+}
+
+#[derive(Deserialize, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct General {
+    pub draft: bool,
+    pub main_file_name: String,
+    pub papersize: String,
+    pub textsize: String,
+}
---
+++ tests/test_generate_template.rs
@@ -0,0 +1,67 @@
+use boilertex::tex::{Documentclass, General, TeXDocument, TeXPackage, TeXTemplate};
+use pretty_assertions::assert_eq;
+use std::collections::HashMap;
+
+const EXAMPLE_TOML_DOCUMENT: &'static str = r#"
+[general]
+draft = true
+main_file_name = "main.tex"
+papersize = "letterpaper"
+textsize = "12pt"
+
+[templates]
+
+[templates.example]
+bib_file = "bibliography.bib"
+documentclass = { class = "article" }
+packages = [
+    { pkg_name = "fontenc", pkg_opts = ["T1"] },
+    { pkg_name = "hyperref", pkg_opts = ["hidelinks", "hypertexnames=false"] },
+    { pkg_name = "graphicx" }
+]
+"#;
+
+#[test]
+fn generate_template() {
+    let serialized_document: TeXDocument = TeXDocument::new(EXAMPLE_TOML_DOCUMENT);
+    let default_document: TeXDocument = TeXDocument::default();
+
+    let test_document: TeXDocument = TeXDocument {
+        general_options: General {
+            draft: true,
+            main_file_name: "main.tex".to_string(),
+            papersize: "letterpaper".to_string(),
+            textsize: "12pt".to_string(),
+        },
+        templates: HashMap::from([(
+            "example".to_string(),
+            TeXTemplate {
+                bib_file: Some("bibliography.bib".to_string()),
+                documentclass: Documentclass {
+                    class: "article".to_string(),
+                    class_opts: None,
+                },
+                packages: Some(vec![
+                    TeXPackage {
+                        pkg_name: "fontenc".to_string(),
+                        pkg_opts: Some(vec!["T1".to_string()]),
+                    },
+                    TeXPackage {
+                        pkg_name: "hyperref".to_string(),
+                        pkg_opts: Some(vec![
+                            "hidelinks".to_string(),
+                            "hypertexnames=false".to_string(),
+                        ]),
+                    },
+                    TeXPackage {
+                        pkg_name: "graphicx".to_string(),
+                        pkg_opts: None,
+                    },
+                ]),
+            },
+        )]),
+    };
+
+    assert_eq!(test_document, default_document);
+    assert_eq!(test_document, serialized_document)
+}
