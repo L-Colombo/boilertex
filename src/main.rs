0846c2481b7abe04fe48bea88962e99eb2316832
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2025-07-26 17:30:15 +0200

    feat(main): add logic to handle cli args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
+use boilertex::{
+    cli::{Cli, Commands},
+    cmds::{genrate_template, list_templates, preview_template},
+    tex::Config,
+};
+use clap::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let cfg = Config::new();
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Generate(args) => genrate_template(cfg, args),
+        Commands::List => list_templates(cfg),
+        Commands::Preview(args) => preview_template(cfg, args.template),
+    }
 }
