47756f4131260871dc333bc4791bd551ad4527a4
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2025-07-26 17:54:02 +0200

    feat(main): add function to generate man pages and shell completion files

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,13 @@ use boilertex::{
     tex::Config,
 };
 use clap::Parser;
+use clap_allgen::*;
+use std::process::exit;
 
 fn main() {
+    // uncomment and run once at every version update
+    // _generate_manpages_and_completions();
+
     let cfg = Config::new();
     let cli = Cli::parse();
 
@@ -14,4 +19,17 @@ fn main() {
         Commands::List => list_templates(cfg),
         Commands::Preview(args) => preview_template(cfg, args.template),
     }
+}
+
+fn _generate_manpages_and_completions() {
+    let current_dir = match std::env::current_dir() {
+        Ok(path) => path,
+        Err(_) => {
+            println!("Could not get current working directory");
+            exit(1)
+        }
+    };
+
+    render_manpages::<Cli>(current_dir.join("etc/man/")).unwrap();
+    render_shell_completions::<Cli>(current_dir.join("etc/shell_comp")).unwrap();
 }
